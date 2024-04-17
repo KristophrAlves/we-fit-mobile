0c63ddff029fbd938b7c12f85fbf44899bcf6c1f
KristophrAlves <kristophralves@gmail.com>
2024-04-17 16:24:57 -0300

    styled.

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,10 +1,4 @@
-import {
-  Linking,
-  Text,
-  TouchableOpacity,
-  View,
-  StyleSheet,
-} from "react-native";
+import { Linking, Text, TouchableOpacity, View } from "react-native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import useList from "../../contexts/ListContext";
@@ -40,86 +34,27 @@ export default function DetailsComponent({ route }) {
         <MaterialCommunityIcons name="circle" color={"#F22828"} size={12} />
         <C.TextLenguage>{language ? language : "Nenhuma"}</C.TextLenguage>
       </C.ContentLenguage>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={{ width: "100%" }}
-          onPress={() => openLink(url)}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-              // backgroundColor: "#1976D2",
-              padding: 12,
-              borderRadius: 8,
-              gap: 8,
-            }}
-          >
+      <C.ButtonContainer>
+        <TouchableOpacity onPress={() => openLink(url)}>
+          <C.ShowView>
             <Text style={{ fontSize: 15, color: "#1976D2" }}>
               VER REPOSITÓRIO
             </Text>
             <Feather name="link-2" size={24} color="#1976D2" />
-          </View>
+          </C.ShowView>
         </TouchableOpacity>
         {route.from == "List" ? (
-          <TouchableOpacity onPress={() => removeRepo()} style={styles.button}>
-            <Text style={styles.buttonText}>FAVORITAR</Text>
+          <C.FavoriteButton onPress={() => removeRepo()}>
+            <C.TextButton>FAVORITAR</C.TextButton>
             <MaterialCommunityIcons name="star" color={"#000000"} size={20} />
-          </TouchableOpacity>
+          </C.FavoriteButton>
         ) : (
-          <TouchableOpacity
-            onPress={() => removeRepo()}
-            style={styles.buttonUnfollow}
-          >
-            <Text style={styles.buttonTextUnfollow}>DESFAVORITAR</Text>
+          <C.UnfavoriteButton onPress={() => removeRepo()}>
+            <C.TextButton>DESFAVORITAR</C.TextButton>
             <Feather name="star" size={24} color="#000000" />
-          </TouchableOpacity>
+          </C.UnfavoriteButton>
         )}
-      </View>
+      </C.ButtonContainer>
     </C.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 16,
-    flexDirection: "column",
-    alignItems: "stretch",
-    backgroundColor: "#ffffff",
-  },
-  button: {
-    flexDirection: "row",
-    backgroundColor: "#FFD02C",
-    padding: 10,
-    borderRadius: 8,
-    marginTop: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-  },
-  buttonUnfollow: {
-    flexDirection: "row",
-    backgroundColor: "#ffffff",
-    padding: 10,
-    borderRadius: 8,
-    marginTop: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-    borderWidth: 1,
-    borderColor: "#000000",
-  },
-  buttonText: {
-    color: "#000000",
-    textAlign: "center",
-  },
-  buttonTextUnfollow: {
-    color: "#000000",
-    textAlign: "center",
-  },
-});
--- src/components/Details/styles.ts
+++ src/components/Details/styles.ts
@@ -34,3 +34,52 @@ export const TextLenguage = styled.Text`
   font-size: 14px;
   color: #9a9a9a;
 `;
+
+export const ShowView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  border-radius: 8px;
+  gap: 8px;
+`;
+
+export const ButtonContainer = styled.View`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 16px;
+  flex-direction: column;
+  align-items: stretch;
+  background-color: #ffffff;
+`;
+
+export const TextButton = styled.Text`
+  color: #000000;
+  text-align: center;
+`;
+
+export const FavoriteButton = styled.TouchableOpacity`
+  flexDirection: "row",
+  backgroundColor: "#FFD02C",
+  padding: 10,
+  borderRadius: 8,
+  marginTop: 10,
+  alignItems: "center",
+  justifyContent: "center",
+  gap: 8,
+`;
+
+export const UnfavoriteButton = styled.TouchableOpacity`
+  flex-direction: row;
+  background-color: #ffffff;
+  padding: 10px;
+  border-radius: 8px;
+  margin-top: 10px;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  border-width: 1px;
+  border-color: #000000;
+`;
